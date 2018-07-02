e741e2a2c2cf1c73e15363564d61a6b7440ddab5
Itai Weisman <iweisman@infinidat.com>
2018-07-02 11:39:29 +0300

    modified:   resize_replica_exlibris.py

--- resize_replica_exlibris.py
+++ resize_replica_exlibris.py
@@ -1,4 +1,9 @@
 from infinisdk import InfiniBox
+from capacity import *
+import base64
+import requests
+import json
+
 #from infinini import 
 
 def get_cg_id(box,cg_name):
@@ -28,9 +33,9 @@ def get_volumes_to_assign(replica):
 
 
 def assign_vols_to_host(remotebox,map_host,vol_list):
-	host=remotebox.hosts.find('name'=map_host)
+	host=remotebox.hosts.find(name=map_host)
 	for vol in vol_list:
-		remote_vol=remotebox.volumes.find('id'=vol)
+		remote_vol=remotebox.volumes.find(id=vol)
 		host.map_volume(remote_vol)
 	return vol_list
 
@@ -41,16 +46,64 @@ def deassign_vols_from_host(map_host,vol_list):
 def resize_volume(box,volume,size):
 	vol=box.volumes.find(name=volume).to_list()
 	if vol:
-		volume=vol[0]
+		return(vol.resize(size))
+
+
+def replica_break(box_ip,local_credentials, remote_credentials, replica):
+	hashed_auth='Basic ' + base64.b64encode(remote_credentials)
+	print hashed_auth
+	url='http://'+box_ip+'/api/rest/replicas/'+str(replica.get_id())+'?approved=true'
+	headers={'X-Remote-Authorization':hashed_auth}
+	return requests.delete(url=url,auth=local_credentials,headers=headers).json()
+
+def get_new_replica_json(old_replica):
+	new_replica={}
+	new_replica['local_cg_id']=old_replica['result']['local_cg_id']
+	new_replica['remote_cg_id']=old_replica['result']['remote_cg_id']
+	new_replica['entity_type']=old_replica['result']['entity_type']
+	new_replica['replication_type']=old_replica['result']['replication_type']
+	new_replica['rpo_type']=old_replica['result']['replication_type']
+	new_replica['rpo_value']=old_replica['result']['rpo_value']
+	new_replica['link_id']=old_replica['result']['link_id']
+	new_replica['entity_pairs']=[]
+	for item in old_replica['result']['entity_pairs']:
+		new_item={}
+		new_item['local_entity_id']=item['local_entity_id']
+		new_item['remote_base_action']=item['remote_base_action']
+		new_item['remote_entity_id']=item['remote_entity_id']
+		new_item['local_base_action']=item['local_base_action']
+		new_replica['entity_pairs'].append(new_item)
+	return new_replica
+
 		
+		
+		
+
+
+	
+
+
+
 if __name__ == '__main__':
 	try:
 	#Init source and target_box
-		cg_id=get_cg_id(box,cg_name)
-		replica=get_cg_replica(box,cg_id)
-		volumes_to_assign=get_volumes_to_assign(replica)
-		vols=assign_vols_to_host(target_box,map_host,volumes_to_assign)
+		#cg_id=get_cg_id(box,cg_name)
+		#replica=get_cg_replica(box,cg_id)
+		#volumes_to_assign=get_volumes_to_assign(replica)
+		#vols=assign_vols_to_host(target_box,map_host,volumes_to_assign)
+		box_name='ibox1499'
+		auth=('iscsi','123456')
+		remote_auth='admin:123456'
+		box_object=InfiniBox(box_name,auth)
+		box_object.login()
+		box_replicas=box_object.replicas.to_list()
+		our_replica=box_object.replicas.get(id='90415')
+		result=replica_break(box_name,auth,remote_auth,our_replica)
+		print result
 		### Idan's part
+	except Exception as E:
+		print "Can't {}".format(E)
+
 
 
 
